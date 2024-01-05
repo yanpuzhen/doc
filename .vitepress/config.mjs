eb05b6a69c977df08911e7e897c376ca5dcf913e
yanpuzhen <yanpuzhen@foxmail.com>
2024-01-05 18:18:38 +0800

    添加碎碎念

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -126,6 +126,7 @@ export default defineConfig({
       { text: '博客', link: 'https://blog.baize.host' },
       { text: '个人主页', link: 'https://bahome.baize.host' },
       { text: '关于', link: '/about' },
+      { text: '唠唠叨叨', link: '/blog/talk.md' },
     ],
 
     sidebar: [ //侧边栏
